47646a434cf919b231e60dbec22a9fcab227cd8e
Yath Seanghay <seanghay.dev@gmail.com>
2020-01-08 15:03:51 +0700

    modified precision

--- studio/src/main/java/com/seanghay/studio/gles/shader/TextureShader.kt
+++ studio/src/main/java/com/seanghay/studio/gles/shader/TextureShader.kt
@@ -147,6 +147,8 @@ open class TextureShader(
 
         // language=glsl
         const val VERTEX_SHADER = """
+            precision mediump float;
+            
             attribute vec4 vPosition;
             attribute vec2 vTextureCoordinate;
 
